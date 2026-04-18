da8dbc87f7f3e64d06e34a98ac570f723d5923ac
Ken <krisken4699@gmail.com>
2026-04-18 10:15:54 +0700

    Nope but this one will

--- src/core/health/health.module.ts
+++ src/core/health/health.module.ts
@@ -1,10 +1,8 @@
 // src/core/health/health.module.ts
 import { Module } from '@nestjs/common';
-import { BucketsModule } from '@/database/buckets/buckets.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [BucketsModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
